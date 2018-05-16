e3199b98b730d10ff859598ef9e5ec1beef514d9
Ryuuichi Iha <ryuuichiiha@RyuuichinoMacBook-puro.local>
2018-05-16 11:28:17 +0900

    farstcommit

---
+++ resources/views/beginner.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>First Page</title>
+</head>
+<body>
+<p>{{$data['name']}}さん</p>
+<p>{{$data['message']}}</p>
+<p>現在は {{$today}} です</p>
+</body>
+</html>
